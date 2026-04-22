f9c8230a7836c2d8028ae3c9f3618b2f4adb7241
ananya <ananyaseayes@gmail.com>
2026-04-22 21:10:33 +0530

    changes made

---
+++ src/main/java/org/bnm/demo/Calculator.java
@@ -0,0 +1,32 @@
+package org.bnm.demo;
+
+public class Calculator {
+	public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+    public static void main(String[] args) {
+
+        Calculator calc = new Calculator();
+
+        int a = 6;
+        int b = 3;
+
+        System.out.println("Addition: " + calc.add(a, b));
+        System.out.println("Subtraction: " + calc.subtract(a, b));
+        System.out.println("Multiplication: " + calc.multiply(a, b));
+        System.out.println("Division: " + calc.divide(a, b));
+    }
+
+}
---
+++ src/test/java/org/bnm/demo/CalculatorTest.java
@@ -0,0 +1,31 @@
+package org.bnm.demo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+
+public class CalculatorTest {
+
+    Calculator calc = new Calculator();
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, calc.add(2,3));
+    }
+
+    @Test
+    public void testSubtract() {
+        assertEquals(2, calc.subtract(5,3));
+    }
+
+    @Test
+    public void testMultiply() {
+        assertEquals(1, calc.multiply(5,3));
+    }
+
+    @Test
+    public void testDivide() {
+        assertEquals(2, calc.divide(6,3));
+    }
+}
